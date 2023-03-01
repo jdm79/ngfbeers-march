2acc16db33a55172a6055742272da47ba420ad9f
jdm79 <james.malvern@gmail.com>
2023-03-01 19:47:50 +0000

    add companies data array file, map over in companies list page

--- pages/companies/index.js
+++ pages/companies/index.js
@@ -1,8 +1,21 @@
+import Link from "next/link";
+import { companies } from "@/companies";
 
 const CompaniesPage = () => {
-  return (
-    <div>Companies Page</div>
-  )
+    
+    return (
+        <div>
+            <h1>Companies Page</h1>
+            <ul>
+               {companies.map((company) => (
+                <li key={company.id}>
+                    <Link href="/companies/id">{company.title}</Link>
+                </li>
+               ))}
+            </ul>
+        </div>
+        
+    )
 }
 
 export default CompaniesPage;
--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,12 @@ const HomePage = () => {
           <li>
             <Link href="/blog">Blog Posts</Link>
           </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
         </ul>
     </div>
   )
