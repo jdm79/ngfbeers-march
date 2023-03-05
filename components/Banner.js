d5055292735278d3586ad3110ee048e5199dc024
jdm79 <james.malvern@gmail.com>
2023-03-05 13:05:13 +0000

    add ga, update message on banner

--- components/Banner.js
+++ components/Banner.js
@@ -4,6 +4,7 @@ function Banner() {
   return (
     <div >
         <h1 className="w-screen flex p-5 justify-center bg-black text-white">
+          UNDER CONSTRUCTION! 
           A showcase of naturally gluten-free (NGCI) beers - beers made without barley or any other gluten-containing ingredients
         </h1>
     </div>
