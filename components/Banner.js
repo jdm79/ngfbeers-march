685b4c4a5e4afb93b2f590a9302ad4c8acb56438
jdm79 <james.malvern@gmail.com>
2023-03-08 15:37:29 +0000

    update banners and hero message

--- components/Banner.js
+++ components/Banner.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-function Banner() {
+function Banner(props) {
   return (
     <div >
-        <h1 className="w-screen flex p-5 justify-center bg-black text-white">
-          UNDER CONSTRUCTION! 
+        <h1 className="w-screen flex p-5 justify-center bg-black text-white text-center">
+          {props.message}
         </h1>
     </div>
   )
--- components/Hero.js
+++ components/Hero.js
@@ -5,17 +5,15 @@ function Hero() {
     <div className="w-screen bg-gray-200 text-black p-10">
         <div>
           <p className="p-2 text-sm">
-            Unlike pretty much all "gluten-free beer" on the UK (and rest of the European) market, 
-            the companies listed here do not use barley or wheat in any of their beers. 
-            In Australia, Canada and the United States, gluten-free beers are those beers made without gluten-containing ingredients.
-            Those beers made with barley and then "deglutenised" are called gluten-reduced beers. 
-       
-            In Europe we call these beers gluten-free, even though the latest scientific research shows that 
-            the deglutenisation process does not work and that testing beer for gluten with the ELISA5 
-            test is inaccurate.
-
-            We have listed the European companies, but will be adding the rest of the world soon.
-
+            
+            Unfortunately, in Europe most gluten-free beers are not actually safe for coeliacs to consume. 
+            Scientific research has shown for a long time that the deglutenisation process does not work 
+            <span className="font-bold"> AND</span> that testing levels of gluten in beer with the ELISA5 test is inaccurate.
+          </p>
+          <p className="p-2 text-sm">
+            This is a health risk, as coeliacs consuming gluten run the risk of infertility, ataxia, 
+            osteoporosis, malnutrition and bowel cancer, to name just a few. 
+            <a href="Potential long-term complications include" className="underline">(source: nhs.uk)</a>
           </p>
         </div>
        
