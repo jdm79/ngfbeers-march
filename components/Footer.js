f9c035ac5b6f63cc3c0c53bd34a0cec9ec4146b8
jdm79 <james.malvern@gmail.com>
2023-03-08 18:39:50 +0000

    add blank tab for fb link, add conditional for beers on company page

--- components/Footer.js
+++ components/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
         <div className="container mx-auto flex justify-center py-12">
             <div className="py-5">
                 <div className="flex gap-6 justify-center">
-                    <Link href={'https://www.facebook.com/profile.php?id=100082957466636'}><ImFacebook /></Link>
+                    <Link href={'https://www.facebook.com/profile.php?id=100082957466636'} target="_blank"><ImFacebook /></Link>
                     <Link href={'https://www.instagram.com/ngfbeers_europe/'}  target="_blank"><ImInstagram /></Link>
                 </div>
                 <p className="py-5 text-gray-800 text-center">Copyright ngfbeers ©2023 All rights reserved</p>
--- pages/companies/[companyId].js
+++ pages/companies/[companyId].js
@@ -83,6 +83,7 @@ function CompanyDetailPage() {
                         <p className="flex items-center -mx-2 text-gray-700 dark:text-gray-200">
                             <span className="mx-2">{company.description}</span>
                         </p>
+                        {company.beers ? (
                         <ul className="grid gap-x-3 row-auto w-200 lg:w-200">
                           {company.beers.map((item) => (
                             <div className="p-5 mt-10  border border-gray-900 dark:border-white">
@@ -105,19 +106,15 @@ function CompanyDetailPage() {
                                  <svg aria-hidden="true" class="w-5 h-5 ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                               </a>
                               </p>
-
-                              
-                             
-                             
-
-
-
                               <p className="flex items-center -mx-2 text-gray-700 dark:text-gray-200">
                                 <span className="mx-2">{item.beer_description}</span>
                               </p>
                             </div>
                           ))}
                         </ul>
+                        ) : (
+                          <div>No beers listed yet</div>
+                        )}
                     </div>
                 </div>
             </div>
