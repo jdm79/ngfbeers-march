076cf54e6e602bff801050258bc91e88946a6726
jdm79 <james.malvern@gmail.com>
2023-03-11 23:11:59 +0000

    update env var names in submit code

--- pages/api/submit.js
+++ pages/api/submit.js
@@ -10,8 +10,8 @@ export default async function handler(req, res) {
     try {
         const auth = new google.auth.GoogleAuth({
             credentials: {
-                client_email: process.env.GOOGLE_CLIENT_EMAIL,
-                private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+                client_email: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_EMAIL,
+                private_key: process.env.NEXT_PUBLIC_GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
             },
             scopes: [
                 'https://www.googleapis.com/auth/drive',
@@ -26,7 +26,7 @@ export default async function handler(req, res) {
         });
 
         const response = await sheets.spreadsheets.values.append({
-            spreadsheetId: process.env.GOOGLE_SHEET_ID,
+            spreadsheetId: process.env.NEXT_PUBLIC_GOOGLE_SHEET_ID,
             range: 'A1:C1',
             valueInputOption: 'USER_ENTERED',
             requestBody: {
