f995d6d7f1e2dc5d16c8a46bfd2b4d4f7f7d97a4
jdm79 <james.malvern@gmail.com>
2023-03-12 20:21:16 +0000

    update tocs

--- pages/tocs.js
+++ pages/tocs.js
@@ -17,13 +17,29 @@ const tocsPage = () => {
             about Your privacy rights and how the law protects You.
             </p>
             <p className="p-5">
-            We use Your Personal data to provide and improve the Service. 
-            By using the Service, 
+            We use Your Personal data to provide and improve the Service.
+            By using the Service,</p>
+            <p className="p-5">
+            We only use Google Analytics to see how many visitors we get, which country they are from,
+            and which pages are most popular. The only personal information we would ever have is
+            if you were to send a message using the contact form. This requires a name, email and message.
+            As explained at the bottom of this page, you can contact us and ask us to delete your 
+            personal data at any time.
+            </p>
+            <p className="p-5"> 
+            We will never sell your data, and we will only use it as described above.
+            </p>
+            <p className="p-5"> 
+            At this time, we do not have user accounts on this website, and so we don't use cookies.
+            If we do add user accounts, we will update this document and cookies will be used to 
+            automatically log in.
+            </p>
+            <p className="p-5"> 
             You agree to the collection and use of information in accordance with this Privacy Policy. 
             This Privacy Policy has been created with the help of the Free Privacy Policy Generator.
             </p>
-            <h1 className="p-5">Interpretation and Definitions</h1>
-            <h2 className="p-5">Interpretation</h2>
+            <h1 className="p-5 font-black">Interpretation and Definitions</h1>
+            <h2 className="p-5 font-black">Interpretation</h2>
             <p className="p-5">
             The words of which the initial letter is capitalized have meanings defined 
             under the following conditions. 
@@ -35,18 +51,7 @@ const tocsPage = () => {
             For the purposes of this Privacy Policy:
             </p>
             <p className="p-5">
-            Account means a unique account created for You to access our Service or parts of our Service.
-            </p>
-            <p className="p-5">
-            Affiliate means an entity that controls, is controlled by or is under common control with a party, where "control" means ownership of 50% or more of the shares, equity interest or other securities entitled to vote for election of directors or other managing authority.
-            </p>
-            <p className="p-5">
             Company (referred to as either "the Company", "We", "Us" or "Our" in this Agreement) refers to ngfbeers.
-            </p>
-            <p className="p-5">
-            Cookies are small files that are placed on Your computer, 
-            mobile device or any other device by a website, 
-            containing the details of Your browsing history on that website among its many uses.
             </p>
             <p className="p-5">
             Country refers to: United Kingdom
@@ -72,74 +77,66 @@ const tocsPage = () => {
             <p className="p-5">
             You means the individual accessing or using the Service, or the company, or other legal entity on behalf of which such individual is accessing or using the Service, as applicable.
             </p>
-            <h1 className="p-5">Collecting and Using Your Personal Data</h1>
-            <p className="p-5">
+            <h1 className="p-5 font-black">Collecting and Using Your Personal Data</h1>
+            <h1 className="p-5 font-black">
             Types of Data Collected
+            </h1>
+            <h1 className="p-5 font-black">
             Personal Data
+            </h1>
+            <p className="p-5">
             While using Our Service, We may ask You to provide Us with certain personally identifiable information that can be used to contact or identify You. Personally identifiable information may include, but is not limited to:
             </p>
-            <h1 className="p-5">
+            <p className="p-5">
             Email address
-            </h1>
-            <h1 className="p-5">
+            </p>
+            <p className="p-5">
             First name and last name
-            </h1>
-            <h1 className="p-5">
+            </p>
+            <p className="p-5">
+            Usage Data
+            </p>
+            <h1 className="p-5 font-black">
             Usage Data
             </h1>
-            <h1 className="p-5">
-            Usage Data
-            </h1>
-
             <p className="p-5">
             Usage Data is collected automatically via Google Analytics when using the Service.
-
             </p>
             <p className="p-5">
             Usage Data may include information such as Your Device's Internet Protocol address (e.g. IP address), browser type, browser version, the pages of our Service that You visit, the time and date of Your visit, the time spent on those pages, unique device identifiers and other diagnostic data.
-
             </p>
             <p className="p-5">
             Your consent to this Privacy Policy followed by Your submission of such information represents Your agreement to that transfer.
-
             </p>
             <p className="p-5">
             The Company will take all steps reasonably necessary to ensure that Your data is treated securely and in accordance with this Privacy Policy and no transfer of Your Personal Data will take place to an organization or a country.
-
             </p>
-            <h1 className="p-5">
+            <h1 className="p-5 font-black">
             Delete Your Personal Data
-
             </h1>
             <p className="p-5">
             You have the right to delete or request that We assist in deleting the Personal Data that We have collected about You.
-
             </p>
-            <h1 className="p-5">
+            <h1 className="p-5 font-black">
             Children's Privacy
-
             </h1>
             <p className="p-5">
             Our Service does not address anyone under the age of 13. We do not knowingly collect personally identifiable information from anyone under the age of 13. If You are a parent or guardian and You are aware that Your child has provided Us with Personal Data, please contact Us. If We become aware that We have collected Personal Data from anyone under the age of 13 without verification of parental consent, We take steps to remove that information from Our servers.
-
             </p>
             <p className="p-5">
             If We need to rely on consent as a legal basis for processing Your information and Your country requires consent from a parent, We may require Your parent's consent before We collect and use that information.
             </p>
-            <h1 className="p-5">
+            <h1 className="p-5 font-black">
             Links to Other Websites
             </h1>
             <p className="p-5">
             Our Service may contain links to other websites that are not operated by Us. If You click on a third party link, You will be directed to that third party's site. We strongly advise You to review the Privacy Policy of every site You visit.
-
             </p>
-            <h1 className="p-5">
+            <h1 className="p-5 font-black">
             Contact Us
-
             </h1>
             <p className="p-5">
             If you have any questions about this Privacy Policy, You can contact us:
-
             </p>
             <p className="p-5">
             By email: 
