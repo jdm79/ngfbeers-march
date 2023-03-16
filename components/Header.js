7be84a710de3ee7c5cf8a953f0cff670ef7df5c9
jdm79 <james.malvern@gmail.com>
2023-03-16 23:43:41 +0000

    remove gradient on footer, add meta tags

--- components/Header.js
+++ components/Header.js
@@ -6,11 +6,13 @@ const Header = () => {
       <Head>
         <title>ngfbeers | naturally gluten-free beers | ngci no barley</title>
         <meta property="og:title" content="ngfbeers | beers made without gluten" key="title" />
+        <meta name="author" content="James Malvern" />
         <meta
           name="description"
           content="A showcase of naturally gluten-free beer companies. ngci beers. made without barley or any other gluten containing ingredients"
           key="desc"
         />
+        <meta name="keywords" content="gluten-free, beer, ngci, barley-free" />
          <meta
           property="og:image"
           content="https://res.cloudinary.com/dlrwbckxi/image/upload/v1678833616/social-media-logo_fahafl.jpg"
