0bc24c777183873dcbdf30899c07685815aa8eb9
jdm79 <james.malvern@gmail.com>
2023-04-10 11:30:11 +0100

    categorise the companies by country, add totals

--- components/companies/companies-list.js
+++ components/companies/companies-list.js
@@ -1,26 +1,36 @@
 import CompanyItem from "./company-item";
 import SocialMediaShare from "../../components/SocialMediaShare";
 import Link from "next/link";
+import CompanyCountry from "../company-country";
 
 function CompaniesList(props) {
   const { items } = props;
   const title = "Coeliac-safe beers";
+  const ausCompanies = items.filter(company => company.country == "Australia")
+  const canCompanies = items.filter(company => company.country == "Canada")
+  const fraCompanies = items.filter(company => company.country == "France")
+  const deCompanies = items.filter(company => company.country == "Germany")
+  const itCompanies = items.filter(company => company.country == "Italy")
+  const ukCompanies = items.filter(company => company.country == "UK")
+  const usaCompanies = items.filter(company => company.country == "USA")
+
 
   return (
     <section className='bg-yellow-400 mb-10 pl-2 pr-2'>
       <div>
         <div className='w-full md:w-3/4 md:max-w-full mt-12 mb-20 mx-auto px-2 sm:px-0'>
           <h1 className='bg-white text-black border-2 border-black rounded-lg p-5 mb-5 shadow-md shadow-black'>
-            This list of gluten-free brewers is constantly being updated and draws heavily upon
-            information from the Zero Tolerance Gluten-Free Brewing Wiki{" "}
+            This list of gluten-free brewers is constantly being updated and
+            draws heavily upon information from the Zero Tolerance Gluten-Free
+            Brewing Wiki{" "}
             <Link
               target='_blank'
               href='https://zerotolerance.mywikis.wiki/wiki/List_of_GF_breweries'
               className='border-2 border-black bg-yellow-400 rounded-lg px-1'
             >
               website
-            </Link>.{" "}
-            If you have any feedback, please use our contact form{" "}
+            </Link>
+            . If you have any feedback, please use our contact form{" "}
             <Link
               href='/contact'
               className='border-2 border-black bg-yellow-400 rounded-lg px-1'
@@ -29,9 +39,14 @@ function CompaniesList(props) {
             </Link>
             .
           </h1>
+          <h1 className='bg-white text-black border-2 border-black rounded-lg p-5 mb-5 shadow-md shadow-black'>
+          Latest number of naturally gluten-free beer companies in the world: {items.length}
+          </h1>
         </div>
-        <ul className='grid grid-cols-2 gap-1 mt-12 xl:mt-12 xl:gap-2 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3 bg-yellow-400'>
-          {items.map((company) => (
+
+        <CompanyCountry country="Australian" amount={ausCompanies.length}/>
+        <ul className='grid grid-cols-2 gap-1 mt-2 xl:gap-2 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3 bg-yellow-400'>
+          {ausCompanies.map((company) => (
             <CompanyItem
               key={company.id}
               id={company.id}
@@ -39,9 +54,106 @@ function CompaniesList(props) {
               title={company.title}
               country={company.country}
               beers={company.beers}
+              city={company.city}
+              state={company.state}
             />
           ))}
         </ul>
+
+        <CompanyCountry country="Canadian" amount={canCompanies.length}/>
+        <ul className='grid grid-cols-2 gap-1 mt-2 xl:gap-2 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3 bg-yellow-400'>
+          {canCompanies.map((company) => (
+            <CompanyItem
+              key={company.id}
+              id={company.id}
+              image={company.image}
+              title={company.title}
+              country={company.country}
+              beers={company.beers}
+              city={company.city}
+              state={company.state}
+            />
+          ))}
+        </ul>
+        <CompanyCountry country="French" amount={fraCompanies.length}/>
+        <ul className='grid grid-cols-2 gap-1 mt-2 xl:gap-2 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3 bg-yellow-400'>
+          {fraCompanies.map((company) => (
+            <CompanyItem
+              key={company.id}
+              id={company.id}
+              image={company.image}
+              title={company.title}
+              country={company.country}
+              beers={company.beers}
+              city={company.city}
+              state={company.state}
+            />
+          ))}
+        </ul>
+        <CompanyCountry country="German" amount={deCompanies.length}/>
+        <ul className='grid grid-cols-2 gap-1 mt-2 xl:gap-2 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3 bg-yellow-400'>
+          {deCompanies.map((company) => (
+            <CompanyItem
+              key={company.id}
+              id={company.id}
+              image={company.image}
+              title={company.title}
+              country={company.country}
+              beers={company.beers}
+              city={company.city}
+              state={company.state}
+            />
+          ))}
+        </ul>
+
+        <CompanyCountry country="Italian" amount={itCompanies.length}/>
+        <ul className='grid grid-cols-2 gap-1 mt-2 xl:gap-2 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3 bg-yellow-400'>
+          {itCompanies.map((company) => (
+            <CompanyItem
+              key={company.id}
+              id={company.id}
+              image={company.image}
+              title={company.title}
+              country={company.country}
+              beers={company.beers}
+              city={company.city}
+              state={company.state}
+            />
+          ))}
+        </ul>
+
+        <CompanyCountry country="UK" amount={ukCompanies.length}/>
+        <ul className='grid grid-cols-2 gap-1 mt-2 xl:gap-2 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3 bg-yellow-400'>
+          {ukCompanies.map((company) => (
+            <CompanyItem
+              key={company.id}
+              id={company.id}
+              image={company.image}
+              title={company.title}
+              country={company.country}
+              beers={company.beers}
+              city={company.city}
+              state={company.state}
+            />
+          ))}
+        </ul>
+
+        <CompanyCountry country="USA" amount={usaCompanies.length}/>
+        <ul className='grid grid-cols-2 gap-1 mt-2 xl:gap-2 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3 bg-yellow-400'>
+          {usaCompanies.map((company) => (
+            <CompanyItem
+              key={company.id}
+              id={company.id}
+              image={company.image}
+              title={company.title}
+              country={company.country}
+              beers={company.beers}
+              city={company.city}
+              state={company.state}
+            />
+          ))}
+        </ul>
+
       </div>
       <div className='w-fit px-5 m-auto'>
         <div className='px-5 mb-4 mt-5'>
--- components/companies/company-item.js
+++ components/companies/company-item.js
@@ -1,41 +1,44 @@
 import Link from "next/link";
 
 function CompanyItem(props) {
-  const { id, title, image, country } = props;
+  const { id, title, image, city, state } = props;
   const companyLink = `/companies/${id}`;
 
   return (
     // <li classNameName="p-2 bg-gray-200 w-full border-b-2 border-black">
     <Link href={companyLink}>
-      <div className="m-auto w-full mb-0">
-        <div className="border-black border-2 bg-white hover:border-4 active:bg-yellow-400">
+      <div className='m-auto w-full mb-0'>
+        <div className='border-black border-2 bg-white hover:border-4 active:bg-yellow-400'>
           <img
-            className="w-3/4 sm:w-1/2 m-auto rounded-full my-4"
+            className='w-3/4 sm:w-1/2 m-auto rounded-full my-4'
             src={`/${image}`}
             alt={title}
           />
-          <div className="flex justify-center  bg-yellow-400 py-2 w-full m-auto border border-t-black">
+          <div className='flex justify-center bg-yellow-400 py-2 w-full m-auto border border-t-black'>
             <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
               strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6 mx-2 text-black"
+              stroke='currentColor'
+              className='w-6 h-6 mx-2 text-black'
             >
               <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M15 10.5a3 3 0 11-6 0 3 3 0 016 0z'
               />
               <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z'
               />
             </svg>
             <span>
-              <h1>{country}</h1>
+              <h1>
+              {city ? `${city}, ` : null}
+              {state ? `${state} ` : null}
+              </h1>
             </span>
           </div>
         </div>
