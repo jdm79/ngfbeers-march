e777327192d857b9d7c39bbd6a1ed6e1580cc05e
jdm79 <james.malvern@gmail.com>
2023-04-10 12:57:04 +0100

    make text smaller on company item city, state div, fix height so all uniform

--- components/company-country.js
+++ components/company-country.js
@@ -4,7 +4,7 @@ function CompanyCountry(props) {
   const { country, amount } = props;
 
   return (
-    <h1 className="mt-12 bg-white border-2 border-black p-4 text-center">{country} Companies ({amount})</h1>
+    <h1 className="mt-12 bg-white border-2 border-black p-4 text-center">{country} Companies: {amount}</h1>
   );
 }
 
