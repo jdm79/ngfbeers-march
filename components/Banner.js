ee88f6a14ac6ace7b42853244c65060cc8f5172e
jdm79 <james.malvern@gmail.com>
2023-04-12 13:16:37 +0100

    fix uk companies list to gb and flag var, change bg to bg-[color:var(--bg-eggshell)]

--- components/Banner.js
+++ components/Banner.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Banner(props) {
   return (
     <div className='shadow-md shadow-black'>
-        <h1 className="p-5 bg-white text-black text-center border-black border-t-2 border-b-2">
+        <h1 className="p-5 bg-[color:var(--bg-eggshell)] text-black text-center border-black border-t-2 border-b-2">
           {props.message}
         </h1>
     </div>
