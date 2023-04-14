1951894ad2294e9f95c56850c716f61308f4d5aa
jdm79 <james.malvern@gmail.com>
2023-04-14 14:30:27 +0100

    styling: add back to beers button to success page

--- pages/success.js
+++ pages/success.js
@@ -1,6 +1,7 @@
 import { getAllCompanies } from "../companies";
 import DefaultLayout from "@/components/DefaultLayout";
 import PageTitle from "@/components/PageTitle";
+import Link from "next/link";
 
 const contactPage = () => {
   const companies = getAllCompanies();
@@ -8,10 +9,17 @@ const contactPage = () => {
   return (
     <DefaultLayout>
       <PageTitle title='Contact Page' />
-      <div className='w-full md:w-3/4 md:max-w-full mt-12 mb-20 mx-auto px-2 sm:px-0 min-h-screen'>
-        <h1 className='bg-white text-center text-black border-2 border-black rounded-lg p-5 mb-5 shadow-md shadow-black'>
-          Form successfully submitted!
-        </h1>
+      <div className='w-full md:w-1/4 md:max-w-full mt-12 mb-20 mx-auto px-2 sm:px-0 min-h-screen'>
+        <div className='bg-white text-center text-black border-2 border-black rounded-lg p-5 mb-5 shadow-md shadow-black'>
+          <div className="mb-20">Form successfully submitted!</div>
+          <div className="p-2 border-2 border-black bg-yellow-400 rounded-lg m-auto w-fit">
+            <Link
+              href='/companies'
+            >
+              Go back to the beers
+            </Link>{" "}
+          </div>
+        </div>
       </div>
     </DefaultLayout>
   );
