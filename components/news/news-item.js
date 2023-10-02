94dc0651d9736afc4c347150eae71e829f49536c
jdm79 <james.malvern@gmail.com>
2023-10-02 15:33:58 +0100

    make links go straight to external, add div above articles, fix the contact us for ngci beers

--- components/news/news-item.js
+++ components/news/news-item.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Link from "next/link";
 
-function newsItem() {
-  return <div>news-item</div>;
+function NewsItem(props) {
+  const { image, url, headline, date } = props;
+
+  return (
+    <Link href={url} target='_blank' className='m-auto w-full'>
+      <div className='bg-gray-100 text-center h-full xl:h-64 w-full xl:w-11/12 p-2'>
+        <img className='w-1/3 p-1' src={image}></img>
+        <div className='h-fit font-sans p-1 text-left m-auto font-bold text-2xl mt-2 bg-white hover:underline'>
+          {headline}
+        </div>
+        <div className='h-fit p-1 text-left m-auto font-bold text-sm mt-1 mb-2'>
+          Published: {date}
+        </div>
+      </div>
+    </Link>
+  );
 }
 
-export default newsItem;
+export default NewsItem;
